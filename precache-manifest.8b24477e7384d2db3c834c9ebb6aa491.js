837adfa584ad4e6540f48e7bd8b1adc4dda39e2c
Ahmed Ebeid <a.ebeid@hotmail.com>
2020-08-21 01:13:06 +0200

    build

---
+++ precache-manifest.8b24477e7384d2db3c834c9ebb6aa491.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed994324a6755e23184e401e415f901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "433b7a45100120e59725",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d862a83480c33b887e1b",
+    "url": "/static/css/main.2a6d9dd5.chunk.css"
+  },
+  {
+    "revision": "433b7a45100120e59725",
+    "url": "/static/js/2.f81a2661.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.f81a2661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d862a83480c33b887e1b",
+    "url": "/static/js/main.2a6a0023.chunk.js"
+  },
+  {
+    "revision": "142af1de3a19a148c46f",
+    "url": "/static/js/runtime-main.0c6910c8.js"
+  },
+  {
+    "revision": "9e8ef348db539e9cdf398cdbc48d8ace",
+    "url": "/static/media/250_50.9e8ef348.png"
+  },
+  {
+    "revision": "0f0c45d0e40758b1bc869bef2427f216",
+    "url": "/static/media/Flaticon.0f0c45d0.svg"
+  },
+  {
+    "revision": "3282636cbac3267d0cb356e9fb840704",
+    "url": "/static/media/Flaticon.3282636c.ttf"
+  },
+  {
+    "revision": "77d626a0826db6fcde573cbe8a8736b3",
+    "url": "/static/media/Flaticon.77d626a0.woff"
+  },
+  {
+    "revision": "c7b874dba932e6bf9da8de4ba9cc4d5c",
+    "url": "/static/media/Flaticon.c7b874db.woff2"
+  },
+  {
+    "revision": "d5c2a38d822f4447b374b9829212c323",
+    "url": "/static/media/Flaticon.d5c2a38d.eot"
+  },
+  {
+    "revision": "d24c8112e1c1707bb996e38731d15d50",
+    "url": "/static/media/HelveticaNeue-Light.d24c8112.woff"
+  },
+  {
+    "revision": "c3b4aff2c6a55cfb0adf9d4498702b47",
+    "url": "/static/media/HelveticaNeue-Medium.c3b4aff2.woff"
+  }
+]);
